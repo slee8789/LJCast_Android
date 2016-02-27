4445e133092e856316b02594a253ff8419a634b7
Seoksoon Jang <jss@koino.net>
2016-02-27 16:07:24 +0900

    add comment for requesting to resolve topbar issue

--- app/src/main/java/android/boraseoksoon/com/ljcast_android/SplashActivity.java
+++ app/src/main/java/android/boraseoksoon/com/ljcast_android/SplashActivity.java
@@ -12,7 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
+
+        // NEED TO REMOVE TOP VIEW LIKE UINavigationBar in iOS
+        /*
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        */
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.splash);
 
         Handler hd = new Handler();
